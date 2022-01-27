083d6c889baa5b0f0b9f1e54c8b10dfd31bfa44f
Simon Penel <simon.penel@univ-lyon1.fr>
2022-01-27 16:11:23 +0100

    Addition of upload page

--- thirdkind_app/thirdkins.js
+++ thirdkind_app/thirdkins.js
@@ -1,11 +1,62 @@
 
 const express = require('express')
 const app = express()
+const path = require("path")
+const multer = require("multer")
 const port = 3000
+// View Engine Setup
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine","ejs")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// var upload = multer({ dest: "Upload_folder_name" })
+// If you do not want to use diskStorage then uncomment it
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        // Uploads is the Upload_folder_name
+        cb(null, "uploads")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now()+".jpg")
+    }
+  })
+
+// Define the maximum size for uploading
+// picture i.e. 1 MB. it is optional
+const maxSize = 1 * 1000 * 1000;
+
+var upload = multer({
+    storage: storage,
+    limits: { fileSize: maxSize },
+    fileFilter: function (req, file, cb){
+
+        // Set the filetypes, it is optional
+        var filetypes = /jpeg|jpg|png/;
+        var mimetype = filetypes.test(file.mimetype);
+
+        var extname = filetypes.test(path.extname(
+                    file.originalname).toLowerCase());
+
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+
+        cb("Error: File upload only supports the "
+                + "following filetypes - " + filetypes);
+      }
+
+// mypic is the name of file attribute
+}).single("mypic");
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+app.get("/",function(req,res){
+    res.render("Signup");
 })
+
 
 const { exec } = require("child_process");
 
