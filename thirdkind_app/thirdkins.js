c677bc08a861021cf41ded9080d5b5474cfff97f
Simon Penel <simon.penel@univ-lyon1.fr>
2022-01-27 15:51:44 +0100

    Test excution of thirdkind: ok

--- thirdkind_app/thirdkins.js
+++ thirdkind_app/thirdkins.js
@@ -7,6 +7,36 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const { exec } = require("child_process");
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("/home/simon/.cargo/bin/thirdkind -h ", (error, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
